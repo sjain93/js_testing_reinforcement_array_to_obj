5c7a95219093be3b4cd2f785ef8837273757b3ed
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-08 15:23:10 -0400

    re complete

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function arrayToObj() {
-     
-}
+// function arrayToObj(array) {
+//   let container = {};
+//   const output = array.map( transformed => {
+//     let director = transformed[0]
+//     container[director] = transformed[1];
+//   });
+//   return container;
+// }
+
+
+// function arrayToObj(array) {
+//   return array.reduce( ( container, transformed) => {
+//     container[transformed[0]] = transformed[1];
+//     return container;
+//   }, {});
+// }
+
+let arrayToObj = array => array.reduce((a, c) => Object.assign(a, { [c[0]]: c[1]}), {})
 
 module.exports = arrayToObj;
